221266baa39cebe9a223657fcceeaf04cb990d97
migueref <rfmo126055@upemor.edu.mx>
2017-02-19 22:09:14 -0600

    Added calculation of resistivity values for earth

--- src/pages/suelo/suelo.ts
+++ src/pages/suelo/suelo.ts
@@ -31,33 +31,40 @@ export class SueloPage {
 		this.model.save()
 	}
   mostrarResultado(number){
-    this.profiles[number].showR=true;
     console.log(this.profiles[number])
+    this.profiles[number].res1=2*3.1416*1*this.profiles[number].med1;
+    this.profiles[number].res2=2*3.1416*1.5*this.profiles[number].med2;
+    this.profiles[number].res3=2*3.1416*2*this.profiles[number].med3;
+    this.profiles[number].res4=2*3.1416*2.5*this.profiles[number].med4;
+    this.profiles[number].res5=2*3.1416*3*this.profiles[number].med5;
+    this.profiles[number].showR=true;
+    this.profiles[number].promedio=(this.profiles[number].res1+this.profiles[number].res2+this.profiles[number].res3+this.profiles[number].res4+this.profiles[number].res5)/5;
+    
   }
   createprofileElements(number){
     this.noPerfiles=number;
     if(number==3){
       this.profiles = [
-        {name:"perfil1",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false},
-  			{name:"perfil2",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false},
-        {name:"perfil3",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false}
+        {name:"perfil1",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false,promedio:0},
+  			{name:"perfil2",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false,promedio:0},
+        {name:"perfil3",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false,promedio:0}
   		];
     }
     if(number==4){
       this.profiles = [
-        {name:"perfil1",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false},
-  			{name:"perfil2",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false},
-        {name:"perfil3",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false},
-  			{name:"perfil3",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false}
+        {name:"perfil1",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false,promedio:0},
+  			{name:"perfil2",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false,promedio:0},
+        {name:"perfil3",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false,promedio:0},
+  			{name:"perfil3",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false,promedio:0}
   		];
     }
     if(number==5){
       this.profiles = [
-        {name:"perfil1",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false},
-  			{name:"perfil2",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false},
-        {name:"perfil3",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false},
-        {name:"perfil3",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false},
-  			{name:"perfil3",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false}
+        {name:"perfil1",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false,promedio:0},
+  			{name:"perfil2",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false,promedio:0},
+        {name:"perfil3",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false,promedio:0},
+        {name:"perfil3",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false,promedio:0},
+  			{name:"perfil3",orientacion:"",med1:0,med2:0,med3:0,med4:0,med5:0,res1:0,res2:0,res3:0,res4:0,res5:0,showR:false,promedio:0}
   		];
     }
 
