90a0843dc087f69676bf658f3931e1fa5d1ba5f9
Miguel Angel Rea Flores <reafmiguel@gmail.com>
2017-02-20 12:36:14 -0600

    jsPDF imported and built a test function to create pdf reports

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+//importing our views
 import {SueloPage} from '../suelo/suelo';
 import {VarillaPage} from '../varilla/varilla';
 import {GrupoPage} from '../grupo/grupo';
-
+//adding jsPDF to generate pdf reports
+import * as jsPDF from 'jspdf';
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
   sueloPage = SueloPage;
@@ -16,5 +18,15 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  download(){
 
+        var doc = new jsPDF();
+        doc.text(20, 20, 'Hello world!');
+        doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+        doc.addPage();
+        doc.text(20, 20, 'Do you like that?');
+
+        // Save the PDF
+        doc.save('Test.pdf');
+  }
 }
